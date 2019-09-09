a5c876993a76894866da2b64542c9ab0c249a9c9
Elizar Pepino <jupenz@gmail.com>
2019-09-09 11:37:43 +0800

    init files

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := ":" + os.Getenv("PORT")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	_ = r.Run(port)
+}
